18bd6b53095745ef2186e1a82730890c22d0a433
Radium Zheng <ztl8702@126.com>
2016-01-30 04:18:59 +0800

    Fixed navbar.

--- www/app/controllers/headerCtrl.js
+++ www/app/controllers/headerCtrl.js
@@ -1,15 +1,15 @@
-(function () {
+(function ($) {
     "use strict";
     angular.module('app').controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $scope.homeClicked = function () {
             $rootScope.$emit('');
             $scope.init = function () {
-                $(".ms-CommandBar").CommandBar();
-            }
+                
+            };
         };
         
         $scope.hamburgerClicked = function () {
             $rootScope.$emit('toggleSidebar');
-        }
+        };
     }]);
-}());
+}(jQuery));
