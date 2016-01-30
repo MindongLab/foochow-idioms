5ce5ad943e6edbca8be6d6887a2ebaf1fd9d54ce
Radium Zheng <ztl8702@126.com>
2016-01-30 16:31:41 +0800

    Fixed navbar. Fixed searchbox.

--- www/app/app.js
+++ www/app/app.js
@@ -4,3 +4,4 @@
     .constant("SERVER_API_URL","http://127.0.0.1:5000/api")
     .constant("SERVER_AUDIO_URL","assets/audio/");
 }());
+
--- www/app/controllers/headerCtrl.js
+++ www/app/controllers/headerCtrl.js
@@ -3,10 +3,12 @@
     angular.module('app').controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $scope.homeClicked = function () {
             $rootScope.$emit('');
-            $scope.init = function () {
-                
-            };
+
         };
+        
+        $scope.init = function () {
+            $(".ms-CommandBar").CommandBar();
+        };     
         
         $scope.hamburgerClicked = function () {
             $rootScope.$emit('toggleSidebar');
