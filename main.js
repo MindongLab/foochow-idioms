dcb1316507fbc43ef81d821530623524487c7dd1
Radium Zheng <ztl8702@msn.com>
2017-05-14 15:23:32 +1000

    add initial structure for electron app

---
+++ main.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({width:800,height:600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname,'www','index.html'),
+        protocal:'file:',
+        slashes:true
+    }))
+
+    console.log(url.format({
+        pathname: path.join(__dirname,'www','index.html'),
+        protocal:'file:',
+        slashes:true
+    }))
+
+    win.on('closed', () => {
+        win=null;
+    });
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate',()=>{
+    if(win===null) {
+        createWindow();
+    }
+});
+
